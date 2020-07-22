bce22142b0c1253ed121e1a99d967d4ddbbc79f7
Ahmad <albastov.ah@gmail.com>
2020-07-22 10:21:29 +0400

    initcommit

--- Java_3_Lesson_1/src/main/java/lesson1/hw1/task3/Test.java
+++ Java_3_Lesson_1/src/main/java/lesson1/hw1/task3/Test.java
@@ -3,7 +3,7 @@ package lesson1.hw1.task3;
 public class Test {
     public static void main(String[] args) {
         Box<Orange> boxO = new Box<>();
-
-        boxO.pourFruit(boxO);
+        Box<Orange> boxO1 = new Box<>();
+        boxO.pourFruit(boxO1);
     }
 }
