9f599f24db221c628ca021d4ebf77e4693b8f460
Ahmad <albastov.ah@gmail.com>
2020-07-22 10:32:48 +0400

    hw1

---
+++ Java_3_Lesson_1/src/main/java/lesson1/hw1/task3/Main.java
@@ -0,0 +1,4 @@
+package lesson1.hw1.task3;
+
+public class Main {
+}
--- Java_3_Lesson_1/src/main/java/lesson1/hw1/task3/Test.java
+++ Java_3_Lesson_1/src/main/java/lesson1/hw1/task3/Test.java
@@ -1,9 +1,13 @@
 package lesson1.hw1.task3;
 
 public class Test {
+
     public static void main(String[] args) {
+
         Box<Orange> boxO = new Box<>();
+
         Box<Orange> boxO1 = new Box<>();
+
         boxO.pourFruit(boxO1);
     }
 }
